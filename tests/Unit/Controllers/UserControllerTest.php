f37c4f45d4cffcf2e3c4aebf897c267edd30b2d6
Leonardo Tramontini <leo.tramontini@gmail.com>
2020-06-06 20:51:36 +0000

    Removing coment test

--- tests/Unit/Controllers/UserControllerTest.php
+++ tests/Unit/Controllers/UserControllerTest.php
@@ -8,11 +8,14 @@ use Vovo\Models\User;
 class UserControllerTest extends TestCase
 {
     protected $baseResource;
+    protected $token;
 
     public function setUp():void
     {
         parent::setUp();
         $this->baseResource = '/api/user';
+        $user = factory(User::class)->create();
+        $this->token = auth()->login($user);
     }
 
     public function testStore()
@@ -43,5 +46,46 @@ class UserControllerTest extends TestCase
             'message',
             'status_code'
         ])->assertStatus(400);
+    }
+
+    public function testUpdate()
+    {
+        $user = factory(User::class)->create();
+
+        $newName = $this->faker->name;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token
+        ])->json('PUT', $this->baseResource . '/' . $user->id, [
+            'name'  => $newName
+        ]);
+
+        $response->assertJson([
+            'data' => [
+                'id'    => $user->id,
+                'name'  => $newName,
+                'email' => $user->email
+            ]
+        ])->assertStatus(200);
+    }
+
+    public function testUpdateShouldBeFail()
+    {
+        $user = factory(User::class)->create();
+
+        $userId = $this->faker->randomDigitNotNull + $user->id;
+
+        $newName = $this->faker->name;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->token
+        ])->json('PUT', $this->baseResource . '/' . $userId, [
+            'name'  => $newName
+        ]);
+
+        $response->assertJsonStructure([
+            'message',
+            'status_code'
+        ])->assertStatus(422);
     }
 }
